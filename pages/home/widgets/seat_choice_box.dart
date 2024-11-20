e58f8462c85f19da157d3db0388158098a9219f0
chaerinyeon <167524718+chaerinyeon@users.noreply.github.com>
2024-11-20 12:42:35 +0900

    동일 역 선택 시 오류
    동일 역 선택 시 오류

--- pages/home/widgets/seat_choice_box.dart
+++ pages/home/widgets/seat_choice_box.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_train_app/pages/seat/seat_page.dart';
 
@@ -34,21 +35,36 @@ class SeatChoiceBox extends StatelessWidget {
         ), //버튼이 활성화 상태 시에만 onPress 함수 실행
         onPressed: isEnabled
             ? () {
-                //출발역과 도착역 출력
-                print('Departure Station: $departureStation');
-                print('Arrival Station: $arrivalStation');
-                Navigator.push(
-                  //SeatPage로 화면 이동
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return SeatPage(
+                // 출발역과 도착역이 같은지 확인
+                if (departureStation == arrivalStation) {
+                  // 출발역과 도착역이 같으면 CupertinoAlertDialog 표시
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                      title: Text('오류'),
+                      content: Text('출발역과 도착역이 같습니다. 다른 역을 선택해주세요.'),
+                      actions: <Widget>[
+                        CupertinoDialogAction(
+                          child: Text('확인'),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  // 출발역과 도착역이 다르면 SeatPage로 이동
+                  print('Departure Station: $departureStation');
+                  print('Arrival Station: $arrivalStation');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SeatPage(
                         departure: departureStation!,
                         arrival: arrivalStation!,
-                      );
-                    },
-                  ),
-                );
+                      ),
+                    ),
+                  );
+                }
               }
             : null, //null 이 아닐 시
         child: Text(
